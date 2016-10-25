daea80e91674d34159bc81b8228126a4a3166860
cerbin <bleble@d.com>
2016-10-25 21:34:45 +0200

    Added method to check if user typed properly option.

--- src/Application.java
+++ src/Application.java
@@ -13,10 +13,24 @@ public class Application {
     private void run() {
         System.out.println("Head or tail?");
         String choice = scanner.nextLine();
+
+        while (true) {
+            if (checkIfHeadOrTail(choice)) {
+                break;
+            } else {
+                System.out.println("Error. Try to type again.");
+                choice = scanner.nextLine();
+            }
+        }
+
         if (choice.equals(coin.randomCoin())) {
             System.out.println("Win :)");
         } else {
             System.out.println("Lose :(");
         }
+    }
+
+    private boolean checkIfHeadOrTail(String stringToCheck) {
+        return stringToCheck.equals("head") || stringToCheck.equals("tail");
     }
 }
