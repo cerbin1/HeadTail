c7d1f1eba918f21894cb43bd10a98a22e100123f
cerbin <bleble@d.com>
2016-11-04 15:43:55 +0100

    Added checkIfUserWantToPlayAnotherGame()

--- src/Application.java
+++ src/Application.java
@@ -5,32 +5,53 @@ public class Application {
     private static Scanner scanner = new Scanner(System.in);
 
     private CoinThrower coin = new CoinThrower();
+    private String answer;
 
     public static void main(String[] args) {
         new Application().run();
     }
 
     private void run() {
-        System.out.println("Head or tail?");
-        String choice = scanner.nextLine();
-
         while (true) {
-            if (checkIfHeadOrTail(choice)) {
-                break;
-            } else {
-                System.out.println("Error. Try to type again.");
-                choice = scanner.nextLine();
-            }
-        }
+            System.out.println("Head or tail?");
+            String choice = scanner.nextLine();
 
-        if (choice.equals(coin.randomCoin())) {
-            System.out.println("Win :)");
-        } else {
-            System.out.println("Lose :(");
+            while (true) {
+                if (checkIfHeadOrTail(choice)) {
+                    break;
+                } else {
+                    System.out.println("Error. Try to type again.");
+                    choice = scanner.nextLine();
+                }
+            }
+
+            if (choice.equals(coin.randomCoin())) {
+                System.out.println("Win :)");
+            } else {
+                System.out.println("Lose :(");
+            }
+
+            if(!checkIfUserWantToPlayAnotherGame()) {
+                break;
+            }
         }
     }
 
     private boolean checkIfHeadOrTail(String stringToCheck) {
         return stringToCheck.equals("head") || stringToCheck.equals("tail");
+    }
+
+    private boolean checkIfUserWantToPlayAnotherGame() {
+        System.out.println("Do you want to play more? yes/no");
+        answer = scanner.nextLine();
+        if (answer.equals("yes")) {
+            return true;
+        } else if (answer.equals("no")) {
+            return false;
+        } else {
+            System.out.println("Type answer again");
+        }
+        checkIfUserWantToPlayAnotherGame();
+        return false;
     }
 }
